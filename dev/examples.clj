cc9161417741dd89ddb868ae000295a0dc65b0d1
Joshua Suskalo <joshua@suskalo.org>
2020-12-31 21:23:27 -0600

    Add spring example

--- dev/examples.clj
+++ dev/examples.clj
@@ -4,7 +4,8 @@
    [examples.window :as window]
    [examples.triangle :as tri]
    [examples.textured-quad :as tex-quad]
-   [examples.gravity :as grav]))
+   [examples.gravity :as grav]
+   [examples.spring :as spring]))
 
 (defn window
   [args]
@@ -18,3 +19,6 @@
 (defn gravity
   [args]
   (grav/start))
+(defn spring
+  [args]
+  (spring/start))
