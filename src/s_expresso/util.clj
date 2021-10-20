0416d698dcb835c8357a1f626ad249133a3049c0
Joshua Suskalo <joshua@suskalo.org>
2021-10-20 18:40:03 -0500

    Add fn arities of when-pred and if-pred

--- src/s_expresso/util.clj
+++ src/s_expresso/util.clj
@@ -6,11 +6,17 @@
   `else` on it.
 
   See also [[when-pred]]."
-  [v pred then else]
-  (if (pred v) (then v) (else v)))
+  ([pred then else]
+   (fn [v]
+     (if-pred v pred then else)))
+  ([v pred then else]
+   (if (pred v) (then v) (else v))))
 
 (defn when-pred
   "If calling `pred` on `v` returns truthy, call `then` on `v`, otherwise return
   it unmodified."
-  [v pred then]
-  (if (pred v) (then v) v))
+  ([pred then]
+   (fn [v]
+     (when-pred v pred then)))
+  ([v pred then]
+   (if (pred v) (then v) v)))
