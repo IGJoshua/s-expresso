35126a70cbbba19a0a9d94b077ef96a860a2f8c3
Joshua Suskalo <joshua@suskalo.org>
2021-10-27 08:13:08 -0500

    Remove the rendering system example
    The rendering system is being used in the other examples now

--- dev/examples.clj
+++ dev/examples.clj
@@ -6,9 +6,7 @@
    [examples.textured-quad :as tex-quad]
    [examples.gravity :as grav]
    [examples.spring :as spring]
-   [examples.events :as ev]
-   ;[examples.rendering-system :as rs]
-   ))
+   [examples.events :as ev]))
 
 (defn window
   [_]
@@ -28,6 +26,3 @@
 (defn events
   [_]
   (ev/start))
-;;(defn rendering-system
-;;  [_]
-;;  (rs/start))
